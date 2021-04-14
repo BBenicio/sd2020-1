fd81a9e4d9b4c4ff5bfe05da1bbeb57b095f4ea2
dalbonio <dalbonio2@gmail.com>
2021-04-13 22:01:33 -0300

    update client.py

--- client.py
+++ client.py
@@ -1,20 +1,32 @@
 import rpyc
+conn = rpyc.connect("localhost", "12345")
 
-conn = rpyc.connect("localhost", 12345)
-
-conn.root.cadastrar_nota(1, 1, 10)
-conn.root.cadastrar_nota(1, 2, 8)
-conn.root.cadastrar_nota(1, 3, 8)
-conn.root.cadastrar_nota(1, 4, 10)
-
-conn.root.cadastrar_nota(2, 1, 10)
-conn.root.cadastrar_nota(2, 2, 8)
-
-conn.root.cadastrar_nota(3, 2, 0)
-
-print(conn.root.consultar_nota(1, 2))
-print(conn.root.consultar_nota(3, 1))
-
-print(conn.root.consultar_notas(1))
-
-print(conn.root.consultar_cr(2))
+running = True
+while running:
+    print("-- Deseja realizar qual operação? 1: Cadastrar nota, 2: Consultar Nota, 3: Consultar Todas as Notas, 4: Consultar CR, 0: Sair --")
+    op = int(input("Operacao: "))
+    if op == 0:
+        running = False
+    elif op == 1:
+        print("Insira a matricula, codigo da disciplina e nota")
+        matricula = input("Matricula: ")
+        cod_disc = input("Codigo da Disciplina: ")
+        nota = input("Nota: ")
+        conn.root.cadastrar_nota(matricula, cod_disc, nota)
+    elif op == 2:
+        print("Insira a matricula e codigo da disciplina")
+        matricula = input("Matricula: ")
+        cod_disc = input("Codigo da Disciplina: ")
+        conn.root.consultar_nota(matricula, cod_disc)
+    elif op == 3:
+        print("Insira a matricula")
+        matricula = input("Matricula: ")
+        conn.root.consultar_nota(matricula)
+    elif op == 4:
+        print("Insira a matricula")
+        matricula = input("Matricula: ")
+        conn.root.consultar_nota(matricula)
+    else:
+        print("Operacao Invalida")
+    #
+#
